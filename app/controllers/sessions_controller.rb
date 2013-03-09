0eea8b8522297e31f82e11abcf7e5de81a46fa73
Matthew Burket <matthew@assignitapp.com>
2013-03-09 14:03:49 -0600

    Make Travis CI work

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by_email(params[:session][:email])
+  	if user.try(:authenticate, params[:session][:password])
+  		session[:user_id] = user.id
+  	else
+  		flash.now[:error] = "The provided email and password did not match with any user on record."
+  		render 'new'
+  	end
+  end
+
+  def destory
+
+  end
 end
