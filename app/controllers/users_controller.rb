f8369ca80de8ac52c24f06c9a9b3c405a1b956ed
Matthew Burket <matthew@assignitapp.com>
2013-03-08 21:30:22 -0600

    Worked on User Controller * Added Controller Action * Added Forms for ** Edit ** New

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,46 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
+     @user = User.find(params[:id])
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "You are now signed up!"
+      redirect_to root_url
+    else
+      render 'new'
+    end
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.update(user_params)
+    if @user.update
+      redirect_to root_url, flash: { success: "User was updated." }
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destory
+    @user.destory
+    redirect_to root_url, flash: { success: "User was destoryed." }
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password)
+    end
 end
