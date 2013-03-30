faff0bf0a31f36bf955d1b34ee5e0e9ea265467a
Matthew Burket <matthew@assignitapp.com>
2013-03-30 17:02:02 -0500

    user_id add comment model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130321030533) do
+ActiveRecord::Schema.define(version: 20130322033718) do
 
   create_table "comments", force: true do |t|
     t.text     "content"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20130321030533) do
     t.string   "ancestry"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
